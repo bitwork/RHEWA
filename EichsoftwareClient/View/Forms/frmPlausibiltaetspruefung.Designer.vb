0b3f7053c81673a6a522180318a6d896f0d58859
bitwork <hill@bitwork.net>
2016-07-08 09:04:49 +0200

    v

--- EichsoftwareClient/View/Forms/frmPlausibiltaetspruefung.Designer.vb
+++ EichsoftwareClient/View/Forms/frmPlausibiltaetspruefung.Designer.vb
@@ -49,6 +49,20 @@ Partial Class frmPlausibiltaetspruefung
         Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.RadGridViewVergleichswerte = New Telerik.WinControls.UI.RadGridView()
         Me.PlausibilitaetDatasourceBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.RadTextBoxControl1 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadTextBoxControl2 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.RadTextBoxControl3 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.RadTextBoxControl4 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.RadTextBoxControl5 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.RadTextBoxControl6 = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.RadTextBoxControlPath, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RadButtonOpen, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RadButtonPruefe, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -59,6 +73,12 @@ Partial Class frmPlausibiltaetspruefung
         CType(Me.RadGridViewVergleichswerte, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RadGridViewVergleichswerte.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PlausibilitaetDatasourceBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBoxControl6, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'OpenFileDialog1
@@ -229,11 +249,139 @@ Partial Class frmPlausibiltaetspruefung
         '
         Me.PlausibilitaetDatasourceBindingSource.DataSource = GetType(EichsoftwareClient.PlausibilitaetDatasource)
         '
+        'RadTextBoxControl1
+        '
+        Me.RadTextBoxControl1.Location = New System.Drawing.Point(102, 378)
+        Me.RadTextBoxControl1.Name = "RadTextBoxControl1"
+        Me.RadTextBoxControl1.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl1.TabIndex = 18
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 382)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Justagepunkt 1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 411)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "Justagepunkt 2"
+        '
+        'RadTextBoxControl2
+        '
+        Me.RadTextBoxControl2.Location = New System.Drawing.Point(102, 407)
+        Me.RadTextBoxControl2.Name = "RadTextBoxControl2"
+        Me.RadTextBoxControl2.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl2.TabIndex = 21
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(233, 382)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 13)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "Kg"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(233, 411)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(20, 13)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Text = "Kg"
+        '
+        'RadTextBoxControl3
+        '
+        Me.RadTextBoxControl3.Location = New System.Drawing.Point(347, 407)
+        Me.RadTextBoxControl3.Name = "RadTextBoxControl3"
+        Me.RadTextBoxControl3.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl3.TabIndex = 27
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(260, 411)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(69, 13)
+        Me.Label9.TabIndex = 26
+        Me.Label9.Text = "Analogwert 2"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(260, 382)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(69, 13)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "Analogwert 1"
+        '
+        'RadTextBoxControl4
+        '
+        Me.RadTextBoxControl4.Location = New System.Drawing.Point(347, 378)
+        Me.RadTextBoxControl4.Name = "RadTextBoxControl4"
+        Me.RadTextBoxControl4.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl4.TabIndex = 24
+        '
+        'RadTextBoxControl5
+        '
+        Me.RadTextBoxControl5.Location = New System.Drawing.Point(102, 433)
+        Me.RadTextBoxControl5.Name = "RadTextBoxControl5"
+        Me.RadTextBoxControl5.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl5.TabIndex = 29
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(15, 437)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 13)
+        Me.Label7.TabIndex = 28
+        Me.Label7.Text = "Nennlast WZ 2"
+        '
+        'RadTextBoxControl6
+        '
+        Me.RadTextBoxControl6.Location = New System.Drawing.Point(347, 433)
+        Me.RadTextBoxControl6.Name = "RadTextBoxControl6"
+        Me.RadTextBoxControl6.Size = New System.Drawing.Size(125, 20)
+        Me.RadTextBoxControl6.TabIndex = 31
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(260, 437)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 13)
+        Me.Label8.TabIndex = 30
+        Me.Label8.Text = "Justagepunkt 2"
+        '
         'frmPlausibiltaetspruefung
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(669, 550)
+        Me.Controls.Add(Me.RadTextBoxControl6)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.RadTextBoxControl5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.RadTextBoxControl3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.RadTextBoxControl4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RadTextBoxControl2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RadTextBoxControl1)
         Me.Controls.Add(Me.RadGridViewVergleichswerte)
         Me.Controls.Add(Me.FlowLayoutPanel1)
         Me.Controls.Add(Me.LabelOption)
@@ -254,6 +402,12 @@ Partial Class frmPlausibiltaetspruefung
         CType(Me.RadGridViewVergleichswerte.MasterTemplate, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.RadGridViewVergleichswerte, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PlausibilitaetDatasourceBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBoxControl6, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -273,4 +427,18 @@ Partial Class frmPlausibiltaetspruefung
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
     Friend WithEvents RadGridViewVergleichswerte As Telerik.WinControls.UI.RadGridView
     Friend WithEvents PlausibilitaetDatasourceBindingSource As BindingSource
+    Friend WithEvents RadTextBoxControl1 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadTextBoxControl2 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents RadTextBoxControl3 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents RadTextBoxControl4 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents RadTextBoxControl5 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents Label7 As Label
+    Friend WithEvents RadTextBoxControl6 As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents Label8 As Label
 End Class
