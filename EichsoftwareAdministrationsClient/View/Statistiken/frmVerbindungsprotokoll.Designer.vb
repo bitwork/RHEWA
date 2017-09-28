31e9da3c81ab559f594abc1dd3f22c0ed8f32fc6
bitwork <hill@bitwork.net>
2017-09-28 09:47:41 +0200

    Verbindungsprotokoll erweitert
    Initial werden nur die top 4000 geladen und über einen Button lässt sich
    der rest laden, da die Tabelle schnell zu groß wird

--- EichsoftwareAdministrationsClient/View/Statistiken/frmVerbindungsprotokoll.Designer.vb
+++ EichsoftwareAdministrationsClient/View/Statistiken/frmVerbindungsprotokoll.Designer.vb
@@ -23,18 +23,22 @@ Partial Class FrmVerbindungsprotokoll
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim TableViewDefinition1 As Telerik.WinControls.UI.TableViewDefinition = New Telerik.WinControls.UI.TableViewDefinition()
         Me.RadGridView1 = New Telerik.WinControls.UI.RadGridView()
+        Me.RadButtonLoadAll = New Telerik.WinControls.UI.RadButton()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.RadGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RadGridView1.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButtonLoadAll, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'RadGridView1
         '
-        Me.RadGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.RadGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.RadGridView1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RadGridView1.Location = New System.Drawing.Point(0, 48)
         '
-        'RadGridView1
+        '
         '
         Me.RadGridView1.MasterTemplate.AllowAddNewRow = False
         Me.RadGridView1.MasterTemplate.AllowCellContextMenu = False
@@ -42,11 +46,31 @@ Partial Class FrmVerbindungsprotokoll
         Me.RadGridView1.MasterTemplate.AllowEditRow = False
         Me.RadGridView1.MasterTemplate.EnableAlternatingRowColor = True
         Me.RadGridView1.MasterTemplate.EnableFiltering = True
+        Me.RadGridView1.MasterTemplate.ViewDefinition = TableViewDefinition1
         Me.RadGridView1.Name = "RadGridView1"
         Me.RadGridView1.ReadOnly = True
-        Me.RadGridView1.Size = New System.Drawing.Size(873, 522)
+        Me.RadGridView1.Size = New System.Drawing.Size(873, 474)
         Me.RadGridView1.TabIndex = 1
         Me.RadGridView1.Text = "RadGridView1"
+        '
+        'RadButtonLoadAll
+        '
+        Me.RadButtonLoadAll.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RadButtonLoadAll.Location = New System.Drawing.Point(751, 13)
+        Me.RadButtonLoadAll.Name = "RadButtonLoadAll"
+        Me.RadButtonLoadAll.Size = New System.Drawing.Size(110, 24)
+        Me.RadButtonLoadAll.TabIndex = 2
+        Me.RadButtonLoadAll.Text = "Alle Laden"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(433, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Die letzen 4000 Einträge. Um alle zu laden bitte auf den Button ""alle Laden"" klic" &
+    "ken"
         '
         'FrmVerbindungsprotokoll
         '
@@ -54,6 +78,8 @@ Partial Class FrmVerbindungsprotokoll
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
         Me.ClientSize = New System.Drawing.Size(873, 522)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadButtonLoadAll)
         Me.Controls.Add(Me.RadGridView1)
         Me.Name = "FrmVerbindungsprotokoll"
         '
@@ -63,10 +89,14 @@ Partial Class FrmVerbindungsprotokoll
         Me.Text = "Verbindungsprotokoll Übersicht"
         CType(Me.RadGridView1.MasterTemplate, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.RadGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButtonLoadAll, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents RadGridView1 As Telerik.WinControls.UI.RadGridView
+    Friend WithEvents RadButtonLoadAll As Telerik.WinControls.UI.RadButton
+    Friend WithEvents Label1 As Label
 End Class
 
--- EichsoftwareAdministrationsClient/View/Statistiken/frmVerbindungsprotokoll.vb
+++ EichsoftwareAdministrationsClient/View/Statistiken/frmVerbindungsprotokoll.vb
@@ -4,40 +4,53 @@ Imports System.ComponentModel
 Public Class FrmVerbindungsprotokoll
 
     Private Sub FrmVerbindungsprotokoll_Load(sender As Object, e As EventArgs) Handles Me.Load
-        LoadFromDatabase()
+        LoadFromDatabase(False)
+        FormatGrid()
 
     End Sub
 
-    Private Sub LoadFromDatabase()
+    Private Sub RadButtonLoadAll_Click(sender As Object, e As EventArgs) Handles RadButtonLoadAll.Click
+        LoadFromDatabase(True)
+    End Sub
+
+    Private Sub LoadFromDatabase(ByVal loadAll As Boolean)
         Using Context As New HerstellerersteichungEntities
-            Dim Data = From Verbindungsprotokoll In Context.ServerVerbindungsprotokoll Select Verbindungsprotokoll
-
+            'Dim Data = From Verbindungsprotokoll In Context.ServerVerbindungsprotokoll Select Verbindungsprotokoll
+            Dim Data As New List(Of ServerVerbindungsprotokoll)
+            If loadAll Then
+                Data.AddRange(Context.ServerVerbindungsprotokoll.ToList)
+            Else
+                Data.AddRange(Context.ServerVerbindungsprotokoll.Take(4000).ToList)
+            End If
             RadGridView1.DataSource = Data.ToList
-            Try
-                RadGridView1.Columns("ID").IsVisible = False
-                RadGridView1.Columns("Lizenzschluessel_FK").HeaderText = "Lizenzschlüssel"
-
-                'Gruppierung
-                Try
-                    Dim descriptor As New GroupDescriptor()
-                    descriptor.GroupNames.Add("Lizenzschluessel_FK", ListSortDirection.Ascending)
-                    Me.RadGridView1.GroupDescriptors.Add(descriptor)
-                Catch e As Exception
-                End Try
-
-                Try
-                    Dim sortdescriptor = New SortDescriptor()
-                    sortdescriptor.Direction = ListSortDirection.Ascending
-                    sortdescriptor.PropertyName = "Timestamp"
-                    Me.RadGridView1.SortDescriptors.Add(sortdescriptor)
-                Catch e As Exception
-
-                End Try
-
-            Catch ex As Exception
-            End Try
-            RadGridView1.AutoExpandGroups = True
-            RadGridView1.BestFitColumns()
         End Using
+    End Sub
+
+    Private Sub FormatGrid()
+        Try
+            RadGridView1.Columns("ID").IsVisible = False
+            RadGridView1.Columns("Lizenzschluessel_FK").HeaderText = "Lizenzschlüssel"
+
+            'Gruppierung
+            Try
+                Dim descriptor As New GroupDescriptor()
+                descriptor.GroupNames.Add("Lizenzschluessel_FK", ListSortDirection.Ascending)
+                Me.RadGridView1.GroupDescriptors.Add(descriptor)
+            Catch e As Exception
+            End Try
+
+            Try
+                Dim sortdescriptor = New SortDescriptor()
+                sortdescriptor.Direction = ListSortDirection.Ascending
+                sortdescriptor.PropertyName = "Timestamp"
+                Me.RadGridView1.SortDescriptors.Add(sortdescriptor)
+            Catch e As Exception
+
+            End Try
+
+        Catch ex As Exception
+        End Try
+        RadGridView1.AutoExpandGroups = True
+        RadGridView1.BestFitColumns()
     End Sub
 End Class
